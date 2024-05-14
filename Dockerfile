9959369ba460554f9e18420a90e929b59e38a170
mevinod <connect@mevinod.com>
2024-05-14 07:05:18 +0000

    fixes - remote user IP

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.10-alpine
 RUN apk update
 RUN apk add --no-cache py3-pip
 RUN mkdir /app
-COPY app.py /app/
+COPY requirements.txt app.py /app/
 WORKDIR /app/
 RUN pip install -r /app/requirements.txt
 EXPOSE 5000
--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+def get_visitor_ip():
+    # Check if request is coming from a proxy
+    if request.headers.get('X-Forwarded-For'):
+        # Return the first IP address in the list (client's IP)
+        return request.headers.get('X-Forwarded-For').split(',')[0]
+    else:
+        # Return the direct IP address of the client
+        return request.remote_addr
+
 @app.route('/')
 def index():
-    ip_address = request.remote_addr
+    ip_address = get_visitor_ip()
     return f"<div style='text-align: center; font-size: 36px; margin-top: 20%;'>Your public IP address is: {ip_address}</div>"
 
 if __name__ == '__main__':
